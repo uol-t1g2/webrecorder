006b5a6ffccc3cbe4874e70c6efa9e9742e04e00
Agam More <agam@agam.me>
2022-12-17 20:06:43 +0200

    Initial: remove unnecessary code from the boilerplate

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,6 +1,7 @@
-import { printLine } from './modules/print';
+document.body.style.backgroundColor = 'red';
+console.log('Testing content script sending a message to popup');
 
-console.log('Content script works!');
-console.log('Must reload extension for modifications to take effect.');
-
-printLine("Using the 'printLine' function from the Print Module");
+chrome.runtime.sendMessage({
+  from: 'content',
+  subject: 'helloFromPage',
+});
--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import logo from '../../assets/img/logo.svg';
 import Greetings from '../../containers/Greetings/Greetings';
 import './Popup.css';
 
 const Popup = () => {
+  useEffect(() => {
+    // Listen for messages from the popup.
+    console.log('Going to listen to events');
+    chrome.runtime.onMessage.addListener((msgObj) => {
+      console.log(msgObj);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/Popup/Popup.jsx</code> and save to reload.
-        </p>
+        <p>Hello UoL class.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
