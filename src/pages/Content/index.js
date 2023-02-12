2fbfe17cc864d3658b4390ff358997b63cd117d7
Battogtokh Baasanjav <toogii@gmail.com>
2023-02-11 17:18:08 -0800

    function for the event trigger is added in Content script

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -4,3 +4,12 @@ chrome.runtime.sendMessage({
   from: 'content',
   subject: 'helloFromPage',
 });
+
+// Create a click function for the content page task
+// The function receives a selector and triggers the click event on it. 
+function click(selector) {
+
+  const element = document.querySelector(selector) || null;
+  if (element)
+    element.click();
+}
