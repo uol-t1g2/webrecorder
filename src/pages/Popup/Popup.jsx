644dd7479bbcb8ee36ea3945629dbf0db9fc0fa3
Battogtokh Baasanjav <toogii@gmail.com>
2023-02-12 17:44:32 -0800

    Button logic is updated as advised by Agam by following React way

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from '../../assets/img/logo_light.svg';
 import Greetings from '../../containers/Greetings/Greetings';
 import './Popup.css';
@@ -6,6 +6,51 @@ import '../../assets/fontawesome/css/fontawesome.min.css';
 import '../../assets/fontawesome/css/solid.min.css';
 
 const Popup = () => {
+  // Button logic
+  const [buttonRecordActive, setButtonRecordActive] = useState(false);
+  const [buttonPlayActive, setButtonPlayActive] = useState(false);
+  const [recordText, setRecordText] = React.useState("Record");
+  const [recordIconClass, setRecordIconClass] = React.useState("fa-solid fa-circle-dot");
+  const [playText, setPlayText] = React.useState("Play");
+  const [playIconClass, setPlayIconClass] = React.useState("fa-solid fa-circle-play");
+  const buttonRecordClass = (buttonRecordActive) ? 'Button-stop' : 'Button-record';
+  const buttonPlayClass = (buttonPlayActive) ? 'Button-stop' : 'Button-play';
+
+  function recordHandler() {
+    if (buttonRecordActive) {
+      setRecordText("Record");
+      setRecordIconClass("fa-solid fa-circle-dot");
+      setButtonRecordActive(false);
+    }
+    else {
+      if (buttonPlayActive) {
+        const element = document.querySelector('#playButton') || null;
+        if (element) element.click();
+      }
+      setRecordText("Stop");
+      setRecordIconClass("fa-solid fa-circle-stop");
+      setButtonRecordActive(true);
+    }
+  }
+
+  function playHandler() {
+    if (buttonPlayActive) {
+      setButtonPlayActive(false);
+      setPlayText("Play");
+      setPlayIconClass("fa-solid fa-circle-play");
+    }
+    else {
+      if (buttonRecordActive) {
+        const element = document.querySelector('#recordButton') || null;
+        if (element) element.click();
+      }
+      setButtonPlayActive(true);
+      setPlayText("Playing");
+      setPlayIconClass("fa-solid fa-stop");
+    }
+  }
+
+
   useEffect(() => {
     // Listen for messages from the popup.
     console.log('Going to listen to events');
@@ -13,50 +58,10 @@ const Popup = () => {
       console.log(msgObj);
     });
 
-    // Button logic to switch on click events
-    document.getElementById('recordButton').addEventListener('click', recClick);
-    document.getElementById('playButton').addEventListener('click', playClick);
-
-    function recClick() {
-      const elRec = document.getElementById('recordButton');
-      const elPlay = document.getElementById('playButton');
-      if (elRec.classList.contains("Button-record")) {
-        if (elPlay.classList.contains("Button-stop")) {
-          elPlay.classList.remove("Button-stop");
-          elPlay.classList.add("Button-play");
-          elPlay.innerHTML = "<i class='fa-solid fa-circle-play'></i> Play";
-        }
-        elRec.classList.remove("Button-record");
-        elRec.classList.add("Button-stop");
-        elRec.innerHTML = "<i class='fa-solid fa-circle-dot'></i> Stop";
-      }
-      else {
-        elRec.classList.remove("Button-stop");
-        elRec.classList.add("Button-record");
-        elRec.innerHTML = "<i class='fa-solid fa-circle-dot'></i> Record";
-      }
-    }
-
-    function playClick() {
-      const elPlay = document.getElementById('playButton');
-      const elRec = document.getElementById('recordButton');
-      if (elPlay.classList.contains("Button-play")) {
-        if (elRec.classList.contains("Button-stop")) {
-          elRec.classList.remove("Button-stop");
-          elRec.classList.add("Button-record");
-          elRec.innerHTML = "<i class='fa-solid fa-circle-dot'></i> Record";
-        }
-        elPlay.classList.remove("Button-play");
-        elPlay.classList.add("Button-stop");
-        elPlay.innerHTML = "<i class='fa-solid fa-circle-stop'></i> Stop";
-      }
-      else {
-        elPlay.classList.remove("Button-stop");
-        elPlay.classList.add("Button-play");
-        elPlay.innerHTML = "<i class='fa-solid fa-circle-play'></i> Play";
-      }
-    }
   }, []);
+
+
+
 
   return (
     <div className="App">
@@ -72,8 +77,8 @@ const Popup = () => {
         </form>
       </div>
       <div className="Button-area">
-        <button className="Button-style Button-record" id="recordButton" type="button"><i className="fa-solid fa-circle-dot"></i> Record</button>
-        <button className="Button-style Button-play" id="playButton" type="button"><i className="fa-solid fa-circle-play"></i> Play</button>
+        <button className={`Button-style ${buttonRecordClass}`} onClick={recordHandler} id="recordButton" type="button"><i className={recordIconClass}></i> {recordText}</button>
+        <button className={`Button-style ${buttonPlayClass}`} onClick={playHandler} id="playButton" type="button"><i className={playIconClass}></i> {playText}</button>
       </div>
     </div>
   );
