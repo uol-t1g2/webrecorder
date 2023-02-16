f60f742a59a03ac41dc4926ddfe7b6594fd4c43f
Agam More <agam@agam.me>
2023-02-16 22:17:40 +0200

    Finish message passing (#12)
    Let me merge it to main and pull latest from main to my other branche

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,15 +1,15 @@
-console.log('Testing content script sending a message to popup');
+// Our global event receiver from Popup
+chrome.runtime.onMessage.addListener(function (msgObj) {
+  console.debug('Got a message from Popup,', msgObj);
+});
 
 chrome.runtime.sendMessage({
-  from: 'content',
-  subject: 'helloFromPage',
+  data: 'Hi from content page!',
 });
 
 // Create a click function for the content page task
-// The function receives a selector and triggers the click event on it. 
+// The function receives a selector and triggers the click event on it.
 function click(selector) {
-
   const element = document.querySelector(selector) || null;
-  if (element)
-    element.click();
+  if (element) element.click();
 }
