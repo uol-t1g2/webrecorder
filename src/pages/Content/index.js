3dc7c8665e19ceff1db55cda3d13bc668c3a748d
YukiChung <m13545022224@163.com>
2023-02-24 12:51:01 +0800

    Plays recording (#15)
    a new function playRecording is implemented in the content script.

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -29,3 +29,9 @@ function click(selector) {
   const element = document.querySelector(selector) || null;
   if (element) element.click();
 }
+// The function plays a recording when needed
+function playRecording(recordedEvents) {
+  recordedEvents.forEach(element => {
+    click(element);
+  });
+}
