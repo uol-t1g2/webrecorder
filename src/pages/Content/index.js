82ac4e878d9fbf32e04251d711d9ecd311139959
Battogtokh Baasanjav <6816035+itoogii@users.noreply.github.com>
2023-03-01 10:11:44 -0800

    implemented the playRecording logic (#23)
    * implemented the setInterval for playing the records in the playRecording function
    
    * created a new function tryClickUntilExists and implemented Promise object to handle the clicks synchronously

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -2,7 +2,7 @@
 import { finder } from '@medv/finder'
 
 // Global recorded events array
-var recordedEvents = [];
+let recordedEvents = [];
 
 // Our global event receiver from Popup
 chrome.runtime.onMessage.addListener(function (msgObj) {
@@ -10,25 +10,24 @@ chrome.runtime.onMessage.addListener(function (msgObj) {
 
   switch (msgObj.action) {
     case 'startRecording':
-      console.log('Should start recording here');
-      sendMessage({ action: 'test', value: 'Hi from content (recorder)!' });
+      console.debug('Should start recording here');
       // attach global event listeners
       attachGlobalEventListeners();
+      sendMessage({ action: 'startedRecording', value: 'Hi from content (recorder)!' });
       break;
     case 'stopRecording':
-      console.log('Should stop recording here');
-      sendMessage({ action: 'test', value: 'Hi from content (recorder)!' });
+      console.debug('Should stop recording here');
+      sendMessage({ action: 'stoppedRecording', value: 'Hi from content (recorder)!' });
       break;
     case 'startPlaying':
-      console.log('Should start playing recording...');
-      sendMessage({ action: 'test', value: 'Hi from content (player)!' });
+      playRecording(recordedEvents);
       break;
     case 'stopPlaying':
-      console.log('Should stop playing recording...');
-      sendMessage({ action: 'test', value: 'Hi from content (player)!' });
+      console.debug('Should stop playing recording...');
+      sendMessage({ action: 'stoppedPlaying', value: 'Hi from content (player)!' });
       break;
     default:
-      console.log('Unkown action of', msgObj.action);
+      console.debug('Unkown action of', msgObj.action);
   }
 });
 
@@ -36,30 +35,19 @@ function sendMessage(message) {
   chrome.runtime.sendMessage(message);
 }
 
-// Create a click function for the content page task
-// The function receives a selector and triggers the click event on it.
-function click(selector) {
-  const element = document.querySelector(selector) || null;
-  if (element) element.click();
-}
-
 // a function that listens to click events and stores them in the recordedEvents array
 function attachGlobalEventListeners() {
-
   // capture all click events
   document.body.addEventListener('click', function (e) {
-
     try {
       // find the best selector for click target (id/class/tag/attr)
       const selector = finder(e.target);
-
       // store selector in recordedEvents
       recordedEvents.push({
         type: 'click',
         element: selector,
         time: new Date().getTime()
       });
-
       // test wether the event was added
       console.debug(
         'array length:', recordedEvents.length,
@@ -74,8 +62,56 @@ function attachGlobalEventListeners() {
 }
 
 // The function plays a recording when needed
-function playRecording(recordedEvents) {
-  recordedEvents.forEach(element => {
-    click(element);
-  });
+async function playRecording(recordedEvents) {
+  for (const event of recordedEvents) {
+    if (event.type == 'click') {
+      try {
+        console.log(event.element, " is caught from events \n", "total events: ", recordedEvents.length);
+        const status = await tryClickUntilExists(event.element, 1000, 5);
+        if (!status) {
+          sendMessage({ action: "finishedPlaying", value: `failed on element ${element}` });
+          return;
+        }
+      } catch (e) {
+        sendMessage({ action: "finishedPlaying", value: `error occured ${e}` });
+      }
+    }
+  }
+  sendMessage({ action: "finishedPlaying", value: "finished playing" });
+}
+
+//Click function that expects selector, interval to retry, maximum retries as params
+function tryClickUntilExists(selector, interval = 400, maxRetries = 1) {
+  return new Promise((resolve, reject) => {
+    let retries = 1;
+    let status = false;
+    //try until click exists
+    let clickInterval = setInterval(() => {
+      const element = document.querySelector(selector) || null;
+      if (element) {
+        try {
+          click(element);
+          status = true;
+        } catch (e) {
+          console.debug('click error: ', e);
+          reject(new Error("failed to click"));
+        }
+      }
+      if (status || (retries >= maxRetries)) {
+        clearInterval(clickInterval);
+        resolve(status);
+      }
+      retries++;
+    }, interval);
+  })
+}
+
+// Create a click function for the content page task
+// The function receives a selector and triggers the click event on it.
+function click(selector) {
+  try {
+    selector.click();
+  } catch (e) {
+    throw e;
+  }
 }
