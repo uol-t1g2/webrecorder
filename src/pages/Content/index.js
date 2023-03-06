f34eacd3d2385fc7dd3df78f473d662728a73068
Battogtokh Baasanjav <toogii@gmail.com>
2023-03-06 13:21:07 -0800

    implemented the detachGlobalEventListener function

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -17,6 +17,8 @@ chrome.runtime.onMessage.addListener(function (msgObj) {
       break;
     case 'stopRecording':
       console.debug('Should stop recording here');
+      // detach global event listeners
+      detachGlobalEventListeners();
       sendMessage({ action: 'stoppedRecording', value: 'Hi from content (recorder)!' });
       break;
     case 'startPlaying':
@@ -38,29 +40,35 @@ function sendMessage(message) {
 // a function that listens to click events and stores them in the recordedEvents array
 function attachGlobalEventListeners() {
   // capture all click events
-  document.body.addEventListener('click', function (e) {
-    try {
-      // find the best selector for click target (id/class/tag/attr)
-      const selector = finder(e.target);
-      // store selector in recordedEvents
-      recordedEvents.push({
-        type: 'click',
-        element: selector,
-        time: new Date().getTime()
-      });
-      // test wether the event was added
-      console.debug(
-        'array length:', recordedEvents.length,
-        '\nlast record:', recordedEvents[recordedEvents.length - 1]
-      );
-    }
-    catch (err) {
-      // in case an element selector could not be found
-      console.debug("oops, we coldn't find a way to select this element");
-    }
-  });
+  document.body.addEventListener('click', listener);
 }
-
+// a function that listens to click events and stores them in the recordedEvents array
+function detachGlobalEventListeners() {
+  // remove the click listener
+  document.body.removeEventListener('click', listener);
+}
+//Function to handle the event listener
+function listener(e) {
+  try {
+    // find the best selector for click target (id/class/tag/attr)
+    const selector = finder(e.target);
+    // store selector in recordedEvents
+    recordedEvents.push({
+      type: 'click',
+      element: selector,
+      time: new Date().getTime()
+    });
+    // test wether the event was added
+    console.debug(
+      'array length:', recordedEvents.length,
+      '\nlast record:', recordedEvents[recordedEvents.length - 1]
+    );
+  }
+  catch (err) {
+    // in case an element selector could not be found
+    console.debug("oops, we coldn't find a way to select this element");
+  }
+}
 // The function plays a recording when needed
 async function playRecording(recordedEvents) {
   for (const event of recordedEvents) {
