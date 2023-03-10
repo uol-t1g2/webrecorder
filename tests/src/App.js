e5a9218a751fc9c38a2bf4bbf8e5f3f7e38d4ea1
zhong yu <m13545022224@163.com>
2023-03-10 15:38:59 +0800

    changed: button id is correspondent to the element name from  textarea of chrome extention

--- tests/src/App.js
+++ tests/src/App.js
@@ -5,19 +5,19 @@ function App() {
   const [buttonName, setButtonName] = useState('');
 
   const handleClick = (event) => {
-    setButtonName(event.target.name);
+    setButtonName(event.target.id);
   };
 
   return (
     <div>
       <h1>Click a Button</h1>
-      <button name="button1" onClick={handleClick}>
+      <button id="button1" onClick={handleClick}>
         Button 1
       </button>
-      <button name="button2" onClick={handleClick}>
+      <button id="button2" onClick={handleClick}>
         Button 2
       </button>
-      <button name="button3" onClick={handleClick}>
+      <button id="button3" onClick={handleClick}>
         Button 3
       </button>
       <p>You clicked: {buttonName}</p>
