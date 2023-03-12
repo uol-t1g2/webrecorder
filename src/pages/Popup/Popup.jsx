eb4579063bcfc641ece620d58f9eb95c2b4c57b2
MariyaShavshy <120510892+MariyaShavshy@users.noreply.github.com>
2023-03-12 10:00:56 -0700

    Popup jsx comments and formatting (#36)
    * change 'Hello UoL Class' placeholder text and slightly stylize it
    
    * changed the cursor style to pointer as buttons are hovered above
    
    * fixed the button-width-per-font-size ratio of the buttons to ensure that both buttons are consistent in size
    
    * changed button colour, highlighted the 'Record' text within the paragraph, restricted logo's width rather than height
    
    * added comments and formatting to popup.css
    
    * adding comments on popup.jsx
    
    * added a new line between 17 and 18, looks better

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -5,33 +5,45 @@ import './Popup.css';
 import '../../assets/fontawesome/css/fontawesome.min.css';
 import '../../assets/fontawesome/css/solid.min.css';
 
+// a function that send messages to the content script
 function sendMessage(message) {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    // send message to the appropreate browser tab
     chrome.tabs.sendMessage(tabs[0].id, message);
   });
 }
 
+// popup page logic
 const Popup = () => {
+
   // Our global event receiver from Content
   chrome.runtime.onMessage.addListener(function (msgObj) {
     console.debug('Got a message from Content,', msgObj);
 
+    // receive messages based on a set of conditions
     switch (msgObj.action) {
+
+      // if the recording was finished
       case 'finishedRecording':
         finishedRecording(msgObj.value);
         break;
+
+      // if the recording finished playing
       case 'finishedPlaying':
         console.log('received finishedPlaying in Popup');
         finishedPlaying(msgObj.value);
         break;
+
+      // otherwise - inform debug console that unknown parameters were received
       default:
         console.debug('Unkown action of', msgObj.action, ' received in Popup');
     }
   });
 
+  // set text content
   const [textContent, setTextContent] = useState('');
 
-  // Button logic
+  // create play button
   const playButton = (
     <button
       className={`Button-style Button-play`}
@@ -42,6 +54,8 @@ const Popup = () => {
       <i className="fa-solid fa-play"></i> Play
     </button>
   );
+
+  // create record button
   const recordButton = (
     <button
       className={`Button-style Button-record`}
@@ -52,6 +66,8 @@ const Popup = () => {
       <i className="fa-solid fa-circle-dot"></i> Record
     </button>
   );
+
+  // create a play-stop button
   const stopPlayButton = (
     <button
       className={`Button-style Button-stop`}
@@ -62,6 +78,8 @@ const Popup = () => {
       <i className="fa-solid fa-stop"></i> Playing
     </button>
   );
+
+  // create a record-stop button
   const stopRecordButton = (
     <button
       className={`Button-style Button-stop`}
@@ -73,24 +91,37 @@ const Popup = () => {
     </button>
   );
 
+  // set active states of buttons
   const [buttonRecordActive, setButtonRecordActive] = useState(false);
   const [buttonPlayActive, setButtonPlayActive] = useState(false);
 
+  // a function to switch between record button states
   function toggleRecordButton() {
+    // if the play button is pressed during the record state
+    // skip record-stop and automatically switch to play
     if (buttonPlayActive) playHandler();
+    // toggle button state
     const newButtonState = !buttonRecordActive;
     setButtonRecordActive(newButtonState);
     chrome.storage.session.set({ recordState: newButtonState });
+    // output a new button state
     return newButtonState;
   }
+
+  // a function to handle a click on the record button
   function recordHandler() {
+    // toggle button state
     const newButtonState = toggleRecordButton();
+
     if (newButtonState) {
+      // if new state is "record"
       sendMessage({
         action: 'startRecording',
         value: 'I want to record events now!',
       });
     } else {
+
+      // if new state is "stop-record"
       sendMessage({
         action: 'stopRecording',
         value: 'I want to stop recording events now!',
@@ -98,17 +129,25 @@ const Popup = () => {
     }
   }
 
+  // a function to handle a click on the play button
   function playHandler() {
+    // if the record button is pressed during the play state
+    // skip play-stop and automatically switch to record
     if (buttonRecordActive) recordHandler();
+
+    // toggle button state
     const newButtonState = !buttonPlayActive;
     setButtonPlayActive(newButtonState);
     chrome.storage.session.set({ playState: newButtonState });
+
     if (newButtonState) {
+      // if new state is "play"
       sendMessage({
         action: 'startPlaying',
         value: 'I want to play events now!',
       });
     } else {
+      // if new state is "stop-play"
       sendMessage({
         action: 'stopPlaying',
         value: 'I want to stop play events now!',
@@ -116,17 +155,16 @@ const Popup = () => {
     }
   }
 
-  // function to handle when the play ended
+  // function to handle the end of playing
   function finishedPlaying(message) {
-    // Handle when play ended
     if (buttonPlayActive) playHandler();
   }
 
-  // function to handle when the record ended
+  // function to handle the end of recording
   function finishedRecording(message) {
-    // Handle when record ended
     if (buttonRecordActive) toggleRecordButton();
-    // Parse the finishedPlaying message on textarea
+    // display the finishedPlaying message on textarea
+    // parse through the recordedEvents objects 
     setTextContent(message);
   }
 
@@ -134,15 +172,21 @@ const Popup = () => {
     // Maintain the Play and Record button states on popup GUI
     chrome.storage.session.get(['recordState'], (result) => {
       const recordButtonState = result.recordState;
+      // if the record button state is undefined
       if (recordButtonState != undefined)
+        // return to initial state
         setButtonRecordActive(recordButtonState);
     });
     chrome.storage.session.get(['playState'], (result) => {
       const playButtonState = result.playState;
-      if (playButtonState != undefined) setButtonPlayActive(playButtonState);
+      // if the play button state is undefined
+      if (playButtonState != undefined)
+        // return to initial state
+        setButtonPlayActive(playButtonState);
     });
   }, []);
 
+  // Generate Popup UI
   return (
     <div className="App">
       <header className="App-header">
